7e394f919a8a3a6639d704dd6a044d2deb091793
ananya09 <ananyamengistu06@gmail.com>
2025-12-18 06:24:47 -0800

    added the html file for each matrec

--- app.py
+++ app.py
@@ -2,21 +2,20 @@ from flask import Flask,render_template, url_for
 app= Flask(__name__)
 @app.route('/')
 def home():
-
     return render_template('index.html')
+
 @app.route('/Length')
 def Length():
-    return "length"
+    return render_template('Length.html')
 
-@app.route('/ Weight')
+@app.route('/Weight')
 def  Weight():
-    return " Weight"
+    return render_template('weght.html')
 
 @app.route('/Temperature')
 def Temperature():
-    return "temperature"
-
-    
+    return render_template('tem.html')
+ 
 
 if __name__== '__main__':
     app.run(debug=True)
