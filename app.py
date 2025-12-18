f39d8820ade5cd13a82f4ad382b93a008c682161
ananya09 <ananyamengistu06@gmail.com>
2025-12-18 04:46:00 -0800

    frist commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, redirect, url_for
+app= Flask(__name__)
+@app.route('/')
+def home():
+    return "hi ananya"
+@app.route('/admin')
+
+def admin():
+    return redirect(url_for('home'))
+
+if __name__== '__main__':
+    app.run()
