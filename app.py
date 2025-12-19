39d40aad7ae4358bf2f618e344510406c42b0db1
ananya09 <ananyamengistu06@gmail.com>
2025-12-19 06:34:05 -0800

    added the form to resive the the data

--- app.py
+++ app.py
@@ -1,20 +1,38 @@
-from flask import Flask,render_template, url_for
+from flask import Flask,render_template, url_for,request
 app= Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/Length')
+@app.route('/Length', methods =['GET','POST'])
 def Length():
-    return render_template('Length.html')
+    if request.method=='POST':
+        ask_len=request.form['value']
+        from_unit=request.form['from_unit']
+        to_unit=request.form['to_unit']
+        return f'{ask_len} {from_unit} {to_unit} this is asked'
+    else:
+        return render_template('Length.html')
 
-@app.route('/Weight')
+@app.route('/Weight', methods =['GET','POST'])
 def  Weight():
-    return render_template('weght.html')
+    if request.method=='POST':
+        ask_weight=request.form['value']
+        from_unit=request.form['from_unit']
+        to_unit=request.form['to_unit']
+        return f'{ask_weight} {from_unit} {to_unit} this is asked'
+    else:
+       return render_template('weght.html')
 
-@app.route('/Temperature')
+@app.route('/Temperature',methods =['GET','POST'])
 def Temperature():
-    return render_template('tem.html')
+    if request.method=='POST':
+        ask_tem=request.form['value']
+        from_unit=request.form['from_unit']
+        to_unit=request.form['to_unit']
+        return f'{ask_tem} {from_unit} {to_unit} this is asked'
+    else:
+      return render_template('tem.html')
  
 
 if __name__== '__main__':
